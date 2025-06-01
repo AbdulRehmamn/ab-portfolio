ddf8a2c5c2ab3b516276d672d36b76137c06d8b8
Abdul Rehman <shahidabdulrehman3434@gmail.com>
2025-06-01 13:21:20 +0500

    added

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -133,15 +133,7 @@ const projects: Project[] = [
     category: ["website", "wATCH sTORE"],
     technologies: ["React", "Tailwind CSS", "Framer Motion"]
 },
-{
-    id: "Text-to-Image",
-    title: "Text-to-Image",
-    description: "A modern website for Text to image genration .",
-    image:"11.png",
-    liveUrl: "https://text-to-image-phi-three.vercel.app/",
-    category: ["website", "Text-to-Image"],
-    technologies: ["React", "Tailwind CSS", "Framer Motion"]
-},
+
 {
     id: "Text to speech",
     title: "Text to speech Website",
